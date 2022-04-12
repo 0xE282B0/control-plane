3c4e2aff78b9560ecc1fa0e38312892a06e25dcc
Sven Pfennig <s.pfennig@reply.de>
2022-04-12 10:14:42 +0200

    use scheduleAfter to calculate maintenance window
    Signed-off-by: Sven Pfennig <s.pfennig@reply.de>

--- components/kyma-environment-broker/internal/orchestration/manager/upgrade_cluster.go
+++ components/kyma-environment-broker/internal/orchestration/manager/upgrade_cluster.go
@@ -117,7 +117,7 @@ func (u *upgradeClusterFactory) CancelOperations(orchestrationID string) error {
 }
 
 // get current retrying operations, update state to pending and update other required params to storage
-func (u *upgradeClusterFactory) RetryOperations(orchestrationID string, schedule orchestration.ScheduleType, policy orchestration.MaintenancePolicy, updateMWindow bool) ([]orchestration.RuntimeOperation, error) {
+func (u *upgradeClusterFactory) RetryOperations(orchestrationID string, schedule orchestration.ScheduleType, policy orchestration.MaintenancePolicy, updateMWindow bool, updateAfter time.Time) ([]orchestration.RuntimeOperation, error) {
 	result := []orchestration.RuntimeOperation{}
 	ops, _, _, err := u.operationStorage.ListUpgradeClusterOperationsByOrchestrationID(orchestrationID, dbmodel.OperationFilter{States: []string{orchestration.Retrying}})
 	if err != nil {
@@ -132,7 +132,7 @@ func (u *upgradeClusterFactory) RetryOperations(orchestrationID string, schedule
 
 			// use the latest policy
 			if schedule == orchestration.MaintenanceWindow {
-				windowBegin, windowEnd, days = resolveMaintenanceWindowTime(op.RuntimeOperation.Runtime, policy)
+				windowBegin, windowEnd, days = resolveMaintenanceWindowTime(op.RuntimeOperation.Runtime, policy, updateAfter)
 			}
 			op.MaintenanceWindowBegin = windowBegin
 			op.MaintenanceWindowEnd = windowEnd
